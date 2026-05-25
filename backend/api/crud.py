eaa7d57d49eb4e6e4c8f074c571afb093b3bb5b8
Michał Nycz <michal223300@gmail.com>
2026-05-25 18:28:23 +0200

    Added /currencies and /currencies/{date} endpoints

--- backend/api/crud.py
+++ backend/api/crud.py
@@ -3,6 +3,26 @@ from sqlalchemy.dialects.postgresql import insert
 from .models import Currency
 from datetime import date
 
+
+def get_currencies(db: Session):
+    return [
+        {"code": row.code, "currency": row.currency}
+        for row in db.query(
+            Currency.code,
+            Currency.currency
+        )
+        .distinct()
+        .order_by(Currency.code)
+        .all()
+    ]
+
+def get_currencies_by_date(db: Session, selected_date: date):
+    return (
+        db.query(Currency)
+        .filter(Currency.rate_date == selected_date)
+        .all()
+    )
+
 def save_currencies(db: Session, currencies_data):
     for item in currencies_data:
         currency = {
--- backend/api/main.py
+++ backend/api/main.py
@@ -8,6 +8,30 @@ from api import crud
 
 app = FastAPI(title="Currencies API")
 
+@app.get("/currencies")
+def get_currencies(db: Session = Depends(get_db)):
+    return crud.get_currencies(db)
+
+
+@app.get("/currencies/{date}")
+def get_currencies_by_date(
+    date: str,
+    db: Session = Depends(get_db)
+):
+    try:
+        parsed_date = datetime.strptime(
+            date,
+            "%Y-%m-%d"
+        ).date()
+
+    except ValueError:
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid date format. Use YYYY-MM-DD"
+        )
+
+    return crud.get_currencies_by_date(db, parsed_date)
+
 @app.post("/currencies/fetch")
 def fetch_currencies(db: Session = Depends(get_db)):
     data = fetch_nbp_data()
