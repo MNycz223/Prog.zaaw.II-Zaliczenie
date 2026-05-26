ce31ed1fac7088a7ce05d7fe6ce2ed27b4bc25a2
Michał Nycz <michal223300@gmail.com>
2026-05-26 08:53:09 +0200

    Fixed page not updating

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
@@ -22,7 +22,7 @@ export class App implements OnInit {
   isLoading = false;
   infoMessage = '';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private cdr: ChangeDetectorRef) { }
 
   ngOnInit(): void {
     this.loadRates();
@@ -31,15 +31,18 @@ export class App implements OnInit {
   // Akcja przycisku: POST /currencies/fetch
   fetchNewRates(): void {
     this.isLoading = true;
+    this.cdr.markForCheck();
     this.http.post<FetchResponse>(`${this.apiUrl}/fetch`, {}).subscribe({
       next: (res) => {
         this.infoMessage = `Sukces: ${res.message} (Pobrano: ${res.count})`;
         this.isLoading = false;
+        this.cdr.markForCheck();
         this.loadRates();
       },
       error: () => {
         this.infoMessage = 'Błąd podczas pobierania danych z zewnętrznego API.';
         this.isLoading = false;
+        this.cdr.markForCheck();
       }
     });
   }
@@ -51,9 +54,11 @@ export class App implements OnInit {
     this.http.get<CurrencyRate[]>(`${this.apiUrl}/${this.selectedDate}`).subscribe({
       next: (data) => {
         this.allRates = data;
+        this.cdr.markForCheck();
       },
       error: () => {
         this.infoMessage = 'Nie udało się załadować kursów walut dla wybranej daty.';
+        this.cdr.markForCheck();
       }
     });
   }
